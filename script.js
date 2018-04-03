61f0a25c317d3dfdac4efc903d78b507e0e8567e
Jen Shannon <jennifer.shannon@galvanize.com>
2018-04-03 10:58:24 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,15 @@
+var myName = "Jen"
+var myFavColor = "Green"
+var myAge = "None of your business"
+
+
+myFunc()
+
+
+function myFunc(){
+  var myFuncVar = "Hey"
+  console.log(myAge)
+}
+
+
+//alert("Your computer has been infected by a virus. Please send bitcoin.");
